c89f9f2fa994bcae6ee2fc584ccbf04389d7e28f
kushal bhalaik <kushal@omnius.com>
2021-04-29 20:52:41 +0530

    New Improvements

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ getBackEndInfo().then(unicornData => {
         `<div class="timeline">
             <div class="timeline-icon"><i class="fa fa-globe"></i></div>
             <div class="timeline-content">
-                <h3 class="title">${currCompany.name} [${currCompany.entry}]</h3>
+            <h3 class="title">${currCompany.name} [${currCompany.entryYear}]</h3>
+            <p class="lead description">${currCompany.description}</p>
               
-                <p class="description">
-                <h3 class="sub-title">${currCompany.valuation} B</h3>
-                Website : ${currCompany.website} Description: ${currCompany.description}
-                  Sector: ${currCompany.sector} , Location: ${currCompany.location} , Investors: ${currCompany.investors} 
+            <p class="lead description">
+                <h3 class="sub-title">Valuation: ${currCompany.valuation} B</h3>
+                
+            Website: <a class="website" href="${currCompany.website}">${currCompany.website}</a>,   Description: ${currCompany.description},
+                Sector: ${currCompany.sector} , Location: ${currCompany.location} , Investors: ${currCompany.investors} 
                 </p>
             </div>
         </div>`
